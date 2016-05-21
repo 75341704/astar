c3f5384bb5dabf29884042e8e9d5661b0752d9ea
Arnout Engelen <arnouten@bzzt.net>
2016-05-21 12:45:09 +0200

    Make astar implementation a bit more readable

--- src/main/scala/Astar.scala
+++ src/main/scala/Astar.scala
@@ -15,22 +15,18 @@ object Astar {
      *  * keep the currentCandidates sorted instead of sorting them every time (faster)
      *  * remember heuristics (faster but more memory)
      *  * remove nodes with a heuristic smaller than the value of the result so far from both the currentCandidates and the alreadyExploded (less memory and probably faster)
-     *
-     * @param currentCandidates not-yet-exploded states
-     * @param resultSoFar highest-valued state exploded so far
-     * @param alreadyExploded already-exploded states (to avoid loops)
      */
     @tailrec
-    def find(currentCandidates: Set[S], resultSoFar: S, alreadyExploded: Set[S]): S = {
-      val sorted = currentCandidates.toList.map(s => (s, optimisticHeuristic(s))).sortBy(_._2).reverse
-
-      if (sorted.isEmpty) resultSoFar
-      else if (sorted.head._2 <= value(resultSoFar)) resultSoFar
-      else {
-        val candidate = sorted.head._1
-        val exploded = explode(candidate) diff alreadyExploded
-        val result = if (value(candidate) > value(resultSoFar)) candidate else resultSoFar
-        find((currentCandidates.filter(_ != candidate) ++ exploded).filter(optimisticHeuristic(_) > value(result)), result, alreadyExploded + candidate)
+    def find(notYetExploded: Set[S], bestSoFar: S, alreadyExploded: Set[S]): S = {
+      notYetExploded.toList.map(s => (s, optimisticHeuristic(s))).sortBy(-_._2).headOption match {
+        case None =>
+          bestSoFar
+        case Some((_, heuristic)) if heuristic <= value(bestSoFar) =>
+          bestSoFar
+        case Some((toExplode, _)) =>
+          val exploded = explode(toExplode) diff alreadyExploded
+          val result = if (value(toExplode) > value(bestSoFar)) toExplode else bestSoFar
+          find((notYetExploded.filter(_ != toExplode) ++ exploded).filter(optimisticHeuristic(_) > value(result)), result, alreadyExploded + toExplode)
       }
     }
 
