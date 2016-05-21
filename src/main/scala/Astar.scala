e8d195136031006f08c9e004d78ff68f0894c60d
Arnout Engelen <arnouten@bzzt.net>
2016-05-21 12:11:46 +0200

    Simplification

--- src/main/scala/Astar.scala
+++ src/main/scala/Astar.scala
@@ -8,6 +8,18 @@ object Astar {
     optimisticHeuristic: S => Int,
     value: S => Int
   ) = {
+    /**
+     * Actually perform the search.
+     *
+     * Possible optimizations:
+     *  * keep the currentCandidates sorted instead of sorting them every time (faster)
+     *  * remember heuristics (faster but more memory)
+     *  * remove nodes with a heuristic smaller than the value of the result so far from both the currentCandidates and the alreadyExploded (less memory and probably faster)
+     *
+     * @param currentCandidates not-yet-exploded states
+     * @param resultSoFar highest-valued state exploded so far
+     * @param alreadyExploded already-exploded states (to avoid loops)
+     */
     @tailrec
     def find(currentCandidates: Set[S], resultSoFar: S, alreadyExploded: Set[S]): S = {
       val sorted = currentCandidates.toList.map(s => (s, optimisticHeuristic(s))).sortBy(_._2).reverse
@@ -15,14 +27,10 @@ object Astar {
       if (sorted.isEmpty) resultSoFar
       else if (sorted.head._2 <= value(resultSoFar)) resultSoFar
       else {
-        val toExplode = sorted.find { case (candidate, _) => !alreadyExploded.contains(candidate) }.map(_._1)
-        toExplode match {
-          case None => resultSoFar
-          case Some(candidate) =>
-            val exploded = explode(candidate) diff alreadyExploded
-            val result = if (value(candidate) > value(resultSoFar)) candidate else resultSoFar
-            find((currentCandidates.filter(_ != candidate) ++ exploded).filter(optimisticHeuristic(_) > value(result)), result, alreadyExploded + candidate)
-        }
+        val candidate = sorted.head._1
+        val exploded = explode(candidate) diff alreadyExploded
+        val result = if (value(candidate) > value(resultSoFar)) candidate else resultSoFar
+        find((currentCandidates.filter(_ != candidate) ++ exploded).filter(optimisticHeuristic(_) > value(result)), result, alreadyExploded + candidate)
       }
     }
 
