bf5bf546744b23803910dd56ad58b41dc4068f06
Arnout Engelen <arnouten@bzzt.net>
2016-05-21 12:50:06 +0200

    Don't remember already-exploded nodes with insufficient heuristic value

--- src/main/scala/Astar.scala
+++ src/main/scala/Astar.scala
@@ -14,7 +14,6 @@ object Astar {
      * Possible optimizations:
      *  * keep the currentCandidates sorted instead of sorting them every time (faster)
      *  * remember heuristics (faster but more memory)
-     *  * remove nodes with a heuristic smaller than the value of the result so far from both the currentCandidates and the alreadyExploded (less memory and probably faster)
      */
     @tailrec
     def find(notYetExploded: Set[S], bestSoFar: S, alreadyExploded: Set[S]): S = {
@@ -26,7 +25,12 @@ object Astar {
         case Some((toExplode, _)) =>
           val exploded = explode(toExplode) diff alreadyExploded
           val result = if (value(toExplode) > value(bestSoFar)) toExplode else bestSoFar
-          find((notYetExploded.filter(_ != toExplode) ++ exploded).filter(optimisticHeuristic(_) > value(result)), result, alreadyExploded + toExplode)
+          val bestValueSoFar = value(result)
+          find(
+            (notYetExploded.filter(_ != toExplode) ++ exploded).filter(optimisticHeuristic(_) > bestValueSoFar),
+            result,
+            (alreadyExploded + toExplode).filter(optimisticHeuristic(_) > bestValueSoFar)
+          )
       }
     }
 
